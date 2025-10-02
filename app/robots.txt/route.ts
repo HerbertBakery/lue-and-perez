e142abe87ac7cdda4615288e40cb99c7116de440
Bird <bird@macbookpro.home>
2025-10-02 10:39:42 -0300

    Use sitemap index + robots.txt pointing to www

--- app/robots.txt/route.ts
+++ app/robots.txt/route.ts
@@ -2,9 +2,8 @@
 export async function GET() {
   const body = `User-agent: *
 Allow: /
+
 Sitemap: https://www.lueandperez.com/sitemap.xml
 `;
-  return new Response(body, {
-    headers: { "Content-Type": "text/plain" },
-  });
+  return new Response(body, { headers: { "Content-Type": "text/plain" } });
 }
